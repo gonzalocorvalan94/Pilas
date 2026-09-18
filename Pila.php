fb00a4a871582d6b547752e0e8bde83aa4e05447
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2026-09-17 21:21:49 -0300

    Se agrego invertir

--- Pila.php
+++ Pila.php
@@ -35,19 +35,22 @@ class Pila
         return empty($this->items);
     }
 
-    public function comparar(Pila $pila1): int
+    public function imprimir(): void
     {
-        $contador1 = $this->contarElementos($this);
-        $contador2 = $this->contarElementos($pila1);
-
-        if ($contador1 > $contador2) {
-            return 1;
+        for ($i = count($this->items) - 1; $i >= 0; $i--) {
+            echo $this->items[$i] . PHP_EOL;
         }
-        if ($contador1 < $contador2) {
-            return -1;
-        }
+    }
 
-        return 0;
+
+    private function invertir():Pila{
+        $aux1 = $this->copiar();
+        $aux2 = new Pila();
+
+        while(!$aux1->vacia()){
+            $aux2->apilar($aux1->desapilar());
+        }
+        return $aux2;
     }
 
     private function contarElementos(Pila $pila): int
@@ -66,7 +69,56 @@ class Pila
         return $contador;
     }
 
-    public function copiar(): Pila
+    private function compararTamanio(Pila $pila1): int
+    {
+        $contador1 = $this->contarElementos($this);
+        $contador2 = $this->contarElementos($pila1);
+
+        if ($contador1 > $contador2) {
+            return 1;
+        }
+        if ($contador1 < $contador2) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    
+
+    private function sonIguales(Pila $pila1): bool
+    {
+        if ($this->compararTamanio($pila1) != 0) {
+            return false;
+        }
+
+        $aux1 = new Pila();
+        $aux2 = new Pila();
+        $flag = true;
+
+        while (!$this->vacia() && !$pila1->vacia()) {
+            $elemento1 = $this->desapilar();
+            $elemento2 = $pila1->desapilar();
+
+            if ($elemento1 != $elemento2) {
+                $flag = false;
+            }
+
+            $aux1->apilar($elemento1);
+            $aux2->apilar($elemento2);
+        }
+
+        while (!$aux1->vacia()) {
+            $this->apilar($aux1->desapilar());
+        }
+        while (!$aux2->vacia()) {
+            $pila1->apilar($aux2->desapilar());
+        }
+
+        return $flag;
+    }
+
+    private function copiar(): Pila
     {
         $aux1 = new Pila();
         $aux2 = new Pila();
@@ -87,10 +139,5 @@ class Pila
         return $copia;
     }
 
-    public function imprimir(): void
-    {
-        for ($i = count($this->items) - 1; $i >= 0; $i--) {
-            echo $this->items[$i] . PHP_EOL;
-        }
-    }
+    
 }
